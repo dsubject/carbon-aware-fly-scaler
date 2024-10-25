f04db6a62200670cba4021d2deeea3974f62f308
Danielle Subject <Danielle.Subject@ibm.com>
2024-10-25 10:50:37 -0400

    deleting machines that aren't best region

--- scale.mjs
+++ scale.mjs
@@ -23,6 +23,9 @@ async function getRankedRegions() {
     const data = await response.json();
     
     console.log("api data", data)
+    console.log("schedule limits", data.regions[0].scheduleLimits)
+    console.log("schedule limits", data.regions[1].scheduleLimits)
+    console.log("schedule limits", data.regions[2].scheduleLimits)
 
     if (!data.regions || data.regions.length === 0) {
       throw new Error('No regions returned by the API');
@@ -40,28 +43,50 @@ async function getRankedRegions() {
   }
 }
 
+// List all machines for the app, filtering based on region
+async function listMachinesByRegion(bestRegion) {
+  try {
+    const { stdout } = await execPromise(`flyctl machines list --json`);
+    const machines = JSON.parse(stdout);
+    
+    
+    console.log("All machines:", machines);
+    
+    // Filter machines by region, returning only those outside bestRegion
+    return machines.map(machine => machine.region);
+
+  } catch (error) {
+    console.error("Error listing machines by region:", error.message);
+    return [];
+  }
+}
+
 // Automate the scaling
 async function automateScaling() {
   const rankedRegions = await getRankedRegions();
+  const allActiveRegions = await listMachinesByRegion();
+
   console.log("ranked", rankedRegions)
+  console.log("active", allActiveRegions)
+  
   if (rankedRegions.length > 0) {
-    await scaleFlyApp(rankedRegions);
+    await scaleFlyApp(rankedRegions, allActiveRegions);
   }
 }
 
 // Scale the Fly.io app based on the best region
+async function scaleFlyApp(rankedRegions, allActiveRegions) {
+  const bestRegion = rankedRegions[0].id;
 
-// To Do: Add a fallback region in case the api fails
-async function scaleFlyApp(rankedRegions) {
-  const bestRegion = rankedRegions[0].id; // Pull region with lowest carbon intensity
+  console.log("best region", bestRegion)
 
-  for (const region of rankedRegions) {
-    const instanceCount = region.id === bestRegion ? 2 : 0; // Scale 2 in the best region, 0 in others
+  for (const region of allActiveRegions) {
+    const instanceCount = region === bestRegion ? 1 : 0; // Scale 1 in the best region, 0 in others
     try {
-      const { stdout } = await execPromise(`flyctl scale count --region ${region.id} ${instanceCount} --process-group worker --yes`);
-      console.log(`Scaled region ${region.id} to ${instanceCount} instances:\n${stdout}`);
+      const { stdout } = await execPromise(`flyctl scale count --region ${region} ${instanceCount} --process-group worker --yes`);
+      console.log(`Scaled region ${region} to ${instanceCount} instances:\n${stdout}`);
     } catch (error) {
-      console.error(`Error scaling region ${region.id}: ${error.message}`);
+      console.error(`Error scaling region ${region}: ${error.message}`);
     }
   }
 }
